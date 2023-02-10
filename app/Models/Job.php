5a3bb20ff9375631e55f7790702b8045bc807f2c
keiyamamura <maki4ma6_w_199san@yahoo.co.jp>
2023-02-10 12:10:01 +0900

    求人テーブル作成

---
+++ app/Models/Job.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'owner_id',
+        'title',
+        'description',
+        'prefectures_id',
+        'status',
+        'wage_type',
+        'salary_amount',
+        'img',
+        'age',
+        'license',
+        'experience',
+        'company_name',
+        'company_tel',
+        'company_email',
+    ];
+}
